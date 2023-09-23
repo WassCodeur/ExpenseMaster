5604122cdd10f12eac2a496cab4d1c2723d96e6e
WassCodeur <wachioubouraima56@gmail.com>
2023-09-23 21:09:51 +0000

    update

---
+++ db.sql
@@ -0,0 +1,47 @@
+-- CREATE DATABASE IF NOT EXISTS `db`;
+DROP DATABASE IF EXISTS `expense_db`;
+CREATE DATABASE expense_db;
+
+USE `expense_db`;
+
+-- Path: db.sql
+
+-- Table structure for table `Users`
+
+DROP TABLE IF EXISTS `Users`;
+CREATE TABLE Users (
+    id INT(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    username VARCHAR(255) NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL,
+    balance DECIMAL(10,2) NOT NULL DEFAULT 0,
+    currency VARCHAR(255) NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    UNIQUE KEY `username` (`username`),
+    UNIQUE KEY `email` (`email`)
+);
+
+-- Table structure for table `Expenses`
+DROP TABLE IF EXISTS `Expenses`;
+CREATE TABLE Expenses (
+    id INT(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    user_id INT(11) NOT NULL,
+    category_id INT(11) NOT NULL,
+    amount DECIMAL(10,2) NOT NULL,
+    description VARCHAR(255) NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    FOREIGN KEY (category_id) REFERENCES Categories(id),
+    FOREIGN KEY (user_id) REFERENCES Users(id)
+);
+
+-- Table structure for table `Categories`
+DROP TABLE IF EXISTS `Categories`;
+CREATE TABLE Categories (
+    id INT(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+);
